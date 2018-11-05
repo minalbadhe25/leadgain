5694deb86b74ce8f57d6e872c697e31df055eeb6
jagdish.khairnar@demandmatrix.com <jagdish.khairnar@demandmatrix.com>
2018-11-05 16:41:20 +0530

    hf

--- src/test/java/com/leadgain/page/ExitIntentPage.java
+++ src/test/java/com/leadgain/page/ExitIntentPage.java
@@ -66,7 +66,7 @@ public class ExitIntentPage extends AbstractPage {
       windowFocus(xpathIframe);
       System.out.println("before click");
 	  waitForElementInVisible(xpathInitLoaderDiv);
-      waitForElementVisible(xpathDownloadBtn, 50 );
+      waitForElementVisible(xpathDownloadBtn, 98 );
       assertAndClick(xpathDownloadBtn);
       System.out.println("after click");
       System.out.println("after focus:");
